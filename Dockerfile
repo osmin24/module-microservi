9da8b5caf8a3cd2facbd97d378401cb243d8ee01
osminPeña <osminv91@gamil.com>
2024-06-09 21:28:34 -0500

    Balancero de carga con nginx

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 401
+EXPOSE 402
 
 CMD ["node","server.js"]
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
  const {app} = require('./app')
 
  app.listen(app.get('port'), () =>{
-    console.log(`http://localhost:${app.get('port')}/cliente/getCliente`)
+    console.log(`http://localhost:${app.get('port')}/`)
  })
